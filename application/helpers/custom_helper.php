949aedfa7e3c8adb80a43bfe997da5fae9bce8c5
asmithadq <asmitha@digitalq.co.in>
2026-07-04 20:26:41 +0530

    Update custom_helper.php
    watermark issue fix

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -537,7 +537,7 @@ function watermarkVideo($input_file,$output_file,$batch_id,$assessor_code,$lat,$
         $box_height = ($lines * $average_char_height) + (($lines - 1) * $line_spacing) + 80; // 40 is padding
         $date_height = $box_height-28;
         $assessor_details_text_height = $date_height-20;
-        $latLong_text_height = $assessor_details_text_height-25;
+        $latLong_text_height = $assessor_details_text_height-20;
         $text_height = $latLong_text_height-12; 
 
         /*echo "<br> content ".$content;
